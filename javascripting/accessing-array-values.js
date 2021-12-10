33483fb0d847cb21f7b16b46ed187c1d12b9af76
FemmeSTEMGem <femmestemgem@gmail.com>
2021-12-10 07:52:41 -0500

    Initial commit with all my work so far

---
+++ javascripting/accessing-array-values.js
@@ -0,0 +1,10 @@
+/*
+In that file, define array food :  
+   
+     const food = ['apple', 'pizza', 'pear']  
+   
+  Use console.log() to print the second value of array to the terminal.  
+*/
+
+let food = ['apple', 'pizza', 'pear'];
+console.log(food[1])
---
+++ javascripting/numbers.js
@@ -0,0 +1,9 @@
+/*
+In that file define a variable named example that references the integer  
+  123456789.  
+   
+  Use console.log() to print that number to the terminal.
+*/
+
+let example = 123456789;
+console.log(example)
